23d8c8bf9123c57766b172cabdf184a8a226cc71
ltwang0618 <136547371+ltwang0618@users.noreply.github.com>
2023-07-16 19:08:08 +0800

    Update homework2-2.py

--- homework2-2.py
+++ homework2-2.py
@@ -1,74 +1,66 @@
 def calculate_sum_of_bonus(data):
-    total_bonus = 0
-    employees = data['employees']
+    allSalary = 0
+    roleBonus = 0
+    baseBonus = 0
+    performanceBonus = 0
+    calSum=0
 
-    i=0
-    while i < len(employees):
-        i+=1
-        name = employees['name']
-        salary = employees['salary']
-        performance = employees['performance']
-        role = employees['role']
-        bonus = individual_bonus(salary, performance, role)
-        employees['bonus'] = bonus
-        total_bonus += bonus
-        
-    def get_salary(salary):
-        if isinstance(salary, str):
-            if "USD" in salary:
-                return float(salary.replace("USD", "")) * 30
-            else:
-                return float(salary)
-        elif isinstance(salary, (int, float)):
-            return salary
+    for object in data["employees"]:
+        defSalary = str(object["salary"])
+        Salary1 = 0
+        Salary2 = 0
+        Salary3 = 0
+
+        if "USD" in defSalary:
+            Salary1 = int(defSalary.replace("USD", "")) * 30
+        elif "," in defSalary:
+            Salary2 = int(defSalary.replace(",", ""))
         else:
-            return 0
+            Salary3 = int(defSalary)
 
-    def individual_bonus(salary, performance, role):
-        base_bonus = get_salary(salary) * 0.02
+        allSalary = (Salary1 + Salary2 + Salary3) * 0.02
+        baseBonus += allSalary
 
-        performance_bonus = 0
-        if performance == "above average":
-            performance_bonus = 2000
-        elif performance == "average":
-            performance_bonus = 1000
-        elif performance == "below average":
-            performance_bonus = 600
+        if object["performance"] == "above average":
+            calSum=2000
+            performanceBonus += calSum
+        elif object["performance"] == "average":
+            calSum=1000
+            performanceBonus += calSum
+        elif object["performance"] == "below average":
+            calSum=600
+            performanceBonus += calSum
 
-        role_bonus = 0
-        if role == "Engineer":
-            role_bonus = base_bonus * 1.2
-        elif role == "CEO":
-            role_bonus = base_bonus * 1.5
-        elif role == "Sales":
-            role_bonus = base_bonus * 1
+        if object["role"] == "Engineer":
+            roleBonus = baseBonus * 0.8
+        elif object["role"] == "CEO":
+            roleBonus = baseBonus * 1
+        elif object["role"] == "Sales":
+            roleBonus = baseBonus * 0.4
 
-        return base_bonus + performance_bonus + role_bonus     
-                
+    totalbonus = baseBonus+ performanceBonus + roleBonus
+    print("Total bonus of all employees:", totalbonus, "TWD")
 
-        print("Total bonus of all employees:", total_bonus, "TWD")    
+calculate_sum_of_bonus({
+    "employees": [
+        {
+            "name": "John",
+            "salary": "1000USD",
+            "performance": "above average",
+            "role": "Engineer"
+        },
+        {
+            "name": "Bob",
+            "salary": 60000,
+            "performance": "average",
+            "role": "CEO"
+        },
+        {
+            "name": "Jenny",
+            "salary": "50,000",
+            "performance": "below average",
+            "role": "Sales"
+        }
+    ]
+})
 
-
-
-        calculate_sum_of_bonus({
-            "employees":[
-                {
-                    "name":"John",
-                    "salary":"1000USD",
-                    "performance":"above average",
-                    "role":"Engineer"
-                },
-                {
-                    "name":"Bob",
-                    "salary":60000,
-                    "performance":"average",
-                    "role":"CEO"
-                },
-                {
-                    "name":"Jenny",
-                    "salary":"50,000",
-                    "performance":"below average",
-                    "role":"Sales"
-                }
-            ]
-        }) # call calculate_sum_of_bonus function
