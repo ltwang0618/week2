a00e4c44b402aafa6b9c7891bcfcc67bb14906d0
ltwang0618 <136547371+ltwang0618@users.noreply.github.com>
2023-07-16 19:09:09 +0800

    Update homework2-3.py

--- homework2-3.py
+++ homework2-3.py
@@ -1,32 +1,28 @@
 def func(*data):
-    middlenames = []
-    fullnames = []
+    middle_names = []
+    full_names = []
 
     for name in data:
-        middlenames.append(name[1])
-        fullnames.append(name)
+        middle_names.append(name[1])
+        full_names.append(name)
 
-    for name1 in data:
-        middlenamecounts = {}
-        for name1 in middlenames:
-            middlenamecounts[name1] = 0
+    uniquename = False
+    for i in range(len(middle_names)):
+        unique_middle_names = True
+        current_middle_name = middle_names[i]
+        for j in range(len(middle_names)):
+            if i != j:
+                if middle_names[j] == current_middle_name:
+                    unique_middle_names = False
+                    break
 
-        for name1 in middlenames:
-            middlenamecounts[name1]+=1 
+        if unique_middle_names:
+            uniquename = True
+            print(full_names[i])
 
-        uniquemiddlenames = {}
-        for name1 in middle_name_counts:
-            if middlenamecounts[name1] == 1:
-                uniquemiddlenames.append(name1)
+    if not uniquename:
+        print("沒有")
         
-        if len(uniquemiddlenames) > 0:
-            for name1 in uniquemiddlenames:
-                for name in fullnames:
-                    if name1 == name[1]:
-                        print(name)
-        else:
-            print("沒有")
-
-    func("彭⼤牆", "王明雅", "吳明") # print 彭⼤牆
-    func("郭靜雅", "王立強", "林靜宜", "郭立恆", "林花花") # print 林花花
-    func("郭宣雅", "林靜宜", "郭宣恆", "林靜花") # print 沒有
+func("彭⼤牆", "王明雅", "吳明") # print 彭⼤牆
+func("郭靜雅", "王立強", "林靜宜", "郭立恆", "林花花") # print 林花 花
+func("郭宣雅", "林靜宜", "郭宣恆", "林靜花") # print 沒有
